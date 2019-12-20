4478e1e7bb4bb3b0abb6ff917e12ccc770e004fc
Muhammad J Siddiq <mjaparsiddiq16@gmail.com>
2019-12-20 14:00:13 +0700

    first commit

---
+++ app/src/main/java/uinbdg/fisip/inputnilai/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package uinbdg.fisip.inputnilai.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uinbdg.fisip.inputnilai.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
